10a8f75f7ab0623f673f351454d24457c4946922
Hayk <aghajanyan.haykv@gmail.com>
2022-05-01 19:42:33 +0400

    INIT

---
+++ src/helpers/constants.js
@@ -0,0 +1,13 @@
+export const COLORS = {
+    RED: 'red',
+    PURPLE: 'purple',
+    BROWN: 'brown',
+    BLUE: 'blue',
+    GREEN: 'green',
+    ORANGE: 'orange',
+}
+
+export const BUTTON_TYPES = {
+    HIDE: 'HIDE',
+    SHOW: 'SHOW'
+}
