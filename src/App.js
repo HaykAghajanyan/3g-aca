b2bb53f5601c3253c6fdcfb61f39068b1842a148
Karpis Karapetyan <karpis.karpis93@gmail.com>
2022-05-06 23:51:52 +0400

    Final_Word

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import { useEffect, useState } from "react";
 
-// Գործնականի համար կպատրաստեք ToDo list -ի ռեալիզացիա։ Այդ պռոյեկտում 
-// https://jsonplaceholder.typicode.com/todos հղումից կներբեռնեք todo-ները։ 
-//  Ամեն todo-ն պետք է ունենա title-ը, id-ն ու status՝ completed ու in progress
-//  և checkbox, որ փոխի այդ ստատուսը։ 
-//  Եթե ստատուսը completed ա պետք է այդ todo-ի գույնը կանաչի։ Նաև պետք է լինի sort
-//  կնոպկա, որը կսորթավորի todo-ները \
-//  ըստ ստատուսի՝ սկզբից ցույց կտա չկատարվածները։ Սթայլերի վրա աշխատելը ցանկալի ա
-
 function App() {
     const [data, setData] = useState([])
+    const [check, setCheck] = useState(Boolean)
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/todos`)
@@ -17,30 +10,22 @@ function App() {
             .then(json => setData(json))
 
     }, [])
-   
-    const [check, setCheck] = useState(Boolean)
 
     return (
         <div className="main">
             <h1 className="title"> Todo List</h1>
-
             <pre>
-
-                {data.slice(0, 10).sort((a,b)=>{
-                   return a.completed>b.completed ? -1 : 1
-                }).map(item => {
-                    return <div key={item.id} className="task"  style={{backgroundColor: item.completed ? 
-                    "rgba(236, 235, 233, 0.5)" : "rgba(247, 235, 211, 0.5)"}}>
-                        <label style={{color: item.completed ? "rgb(33, 141, 33)" : "red"}}>
-                        <input type="checkbox" onClick={() => {
-                            setCheck(prev => !prev)
-                            item.completed =  !item.completed    
-                        }} />{item.title} : {String(item.completed)}
-
-                    </label></div>
+                {data.slice(0, 10)
+                .sort((a,b) => a.completed > b.completed ? 1 : -1)
+                .map(item => {
+                    return <div key={item.id} className="task"  
+                            style={{ backgroundColor: item.completed ? "#B9E28C" : "#EDAE49"}}>
+                    <label style={{ color: item.completed ? "#1c8d21" : "#DB2B39"}}>
+                    <input type="checkbox" onClick={() => {setCheck(prev => !prev)
+                            item.completed =  !item.completed}}/>
+                    {item.title} : {!item.completed ? "in Progress" : "Completed" }</label></div>
                 })
                 }
-
             </pre>
         </div>
     )
