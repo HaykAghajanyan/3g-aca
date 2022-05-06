b7cc51a5ba2c1e8b54868d680727d9fae885493d
mari-khachatryan <mariam.khachatryan.89.m@gmail.com>
2022-05-06 19:42:52 +0400

    change App class component to functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component} from "react";
+import React, { useState } from "react";
 import {COLORS, BUTTON_TYPES} from './helpers/constants'
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer";
@@ -7,85 +7,80 @@ import NavBar from "./components/NavBar";
 const {RED, PURPLE, BLUE, BROWN, GREEN, ORANGE} = COLORS
 const {HIDE, SHOW} = BUTTON_TYPES
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            circles: [
-                {
-                    id: '1',
-                    color: RED
-                },
-                {
-                    id: '2',
-                    color: PURPLE
-                },
-                {
-                    id: '3',
-                    color: BROWN
-                },
-                {
-                    id: '4',
-                    color: BLUE
-                },
-                {
-                    id: '5',
-                    color: GREEN
-                },
-            ],
-            chosenCircle: null,
-            isHeaderShown: true,
-            randomNumFromHeader: null
-        }
+function App() {
+    const [state, setState] = useState({
+        circles: [
+            {
+                id: '1',
+                color: RED
+            },
+            {
+                id: '2',
+                color: PURPLE
+            },
+            {
+                id: '3',
+                color: BROWN
+            },
+            {
+                id: '4',
+                color: BLUE
+            },
+            {
+                id: '5',
+                color: GREEN
+            },
+        ],
+        chosenCircle: null,
+        isHeaderShown: true,
+        randomNumFromHeader: null
+    })
+
+    const changeColor = e => {
+        setState({chosenCircle: e.target.id})
     }
 
-    changeColor = e => {
-        this.setState({chosenCircle: e.target.id})
+    const toggleHeader = () => {
+        setState(prev => ({
+            ...prev,
+            isHeaderShown: !prev.isHeaderShown
+        }))
     }
 
-    toggleHeader = () => {
-        this.setState(prev => {
-            prev.isHeaderShown = !prev.isHeaderShown
-            return prev
-        })
-    }
-
-    getRandomNum = (num) => {
-        this.setState({randomNumFromHeader: num})
+    const getRandomNum = (num) => {
+        setState(prev => ({...prev, randomNumFromHeader: num}))
     }
 
 
-    render() {
-        const {circles, chosenCircle, isHeaderShown, randomNumFromHeader} = this.state
+    const {circles, chosenCircle, isHeaderShown, randomNumFromHeader} = state
 
-        console.log('randomNumFromHeader', randomNumFromHeader)
-        return (
-            <>
-                {isHeaderShown && <Header
-                    getRandomNum={this.getRandomNum}
-                    color={chosenCircle && circles[chosenCircle - 1].color}
-                />}
+    console.log('randomNumFromHeader', randomNumFromHeader)
+    return (
+        <>
+            {isHeaderShown && <Header
+                getRandomNum={getRandomNum}
+                color={chosenCircle && circles[chosenCircle - 1].color}
+            />}
 
 
-                <button onClick={this.toggleHeader}>{isHeaderShown ? HIDE : SHOW}</button>
-                <div className="container">
-                    {
-                        circles.map(circle => {
-                            return <div
-                                key={circle.id}
-                                id={circle.id}
-                                className='circle'
-                                style={{backgroundColor: chosenCircle === circle.id ? ORANGE : circle.color}}
-                                onClick={this.changeColor}
-                            >{circle.id}</div>
-                        })
-                    }
-                </div>
-                <Footer />
-                <NavBar />
-            </>
-        );
-    }
+            <button onClick={toggleHeader}>{isHeaderShown ? HIDE : SHOW}</button>
+            <div className="container">
+                {
+                    circles.map(circle => {
+                        return <div
+                            key={circle.id}
+                            id={circle.id}
+                            className='circle'
+                            style={{backgroundColor: chosenCircle === circle.id ? ORANGE : circle.color}}
+                            onClick={changeColor}
+                        >{circle.id}</div>
+                    })
+                }
+            </div>
+            <Footer />
+            <NavBar />
+        </>
+    );
 }
 
 export default App;
