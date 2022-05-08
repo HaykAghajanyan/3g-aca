9e08535e85d02a65a4b56775a1939f1cf9951a52
Hayk <aghajanyan.haykv@gmail.com>
2022-05-08 17:24:00 +0400

    Hyak commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 function App() {
     const [data, setData] = useState([])
@@ -7,24 +7,32 @@ function App() {
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/todos`)
             .then(res => res.json())
-            .then(json => setData(json))
-
+            .then(json => setData(json.slice(0, 10)))
     }, [])
 
     return (
         <div className="main">
             <h1 className="title"> Todo List</h1>
             <pre>
-                {data.slice(0, 10)
-                .sort((a,b) => a.completed > b.completed ? 1 : -1)
-                .map(item => {
-                    return <div key={item.id} className="task"  
-                            style={{ backgroundColor: item.completed ? "#B9E28C" : "#EDAE49"}}>
-                    <label style={{ color: item.completed ? "#1c8d21" : "#DB2B39"}}>
-                    <input type="checkbox" onClick={() => {setCheck(prev => !prev)
-                            item.completed =  !item.completed}}/>
-                    {item.title} : {!item.completed ? "in Progress" : "Completed" }</label></div>
-                })
+                {data
+                    .sort((a, b) => a.completed > b.completed ? 1 : -1)
+                    .map(item => {
+                        return (
+                            <div
+                                key={item.id}
+                                className="task"
+                                style={{backgroundColor: item.completed ? "#B9E28C" : "#EDAE49"}}
+                            >
+                                <label style={{color: item.completed ? "#1c8d21" : "#DB2B39"}}>
+                                    <input type="checkbox" onClick={() => {
+                                        setCheck(prev => !prev)
+                                        item.completed = !item.completed
+                                    }}/>
+                                    {item.title} : {!item.completed ? "in Progress" : "Completed"}
+                                </label>
+                            </div>
+                        )
+                    })
                 }
             </pre>
         </div>
